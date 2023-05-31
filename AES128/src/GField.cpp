fb91700ec02b8c6035d3433cdc2793dd8aca86c3
pderbez <48960153+pderbez@users.noreply.github.com>
2023-05-31 12:06:05 +0200

    Update GField.cpp

--- AES128/src/GField.cpp
+++ AES128/src/GField.cpp
@@ -1,7 +1,6 @@
 /*
  * GField.cpp
  * 
- * Copyright 2014 Patrick Derbez <patrick.derbez@uni.lu>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
